b240ec5bcf088d11f9fb602cb0b1c0a372a2b065
sdtvAmrit <amrit.srivastava@smardtv.com>
2023-09-03 18:53:32 +0530

    commiting

---
+++ app/src/main/java/com/wild/smartrack/ui/fragments/ConfirmImage.kt
@@ -0,0 +1,20 @@
+package com.wild.smartrack.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.wild.smartrack.R
+
+class ConfirmImage : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_confirm_image, container, false)
+    }
+
+}
