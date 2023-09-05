4f520d8af0f3ce5d5edbb6aa1de89a51fdccbe62
Cizor <yourcizor@gmail.com>
2023-09-05 09:55:11 +0530

    ConfirmImage start

--- app/src/main/java/com/wild/smartrack/ui/fragments/ConfirmImage.kt
+++ app/src/main/java/com/wild/smartrack/ui/fragments/ConfirmImage.kt
@@ -5,16 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.wild.smartrack.R
+import com.wild.smartrack.databinding.FragmentConfirmImageBinding
+import com.wild.smartrack.viewmodels.ListOfHubsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ConfirmImage : Fragment() {
-
+    //Shared one
+    private val listOfHubsViewModel: ListOfHubsViewModel by activityViewModels()
+    private var _binding: FragmentConfirmImageBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_confirm_image, container, false)
+        _binding = FragmentConfirmImageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
